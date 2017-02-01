34971a40aeb84892c6c4715e2a8cfcacb288ecc3
Kunal Patel <kunalpatelx92@gmail.com>
2017-01-31 18:53:58 -0600

    Optional Matching with the Question Mark

--- Regular_Expressions/reModule.py
+++ Regular_Expressions/reModule.py
@@ -19,14 +19,29 @@ import re
 # print(mainNumber)
 
 # matching multiple groups with the pipe
-heroRegex = re.compile(r'Batman|Tina Fey')
-mo1 = heroRegex.search('Batman and Tina Fey.')
+# heroRegex = re.compile(r'Batman|Tina Fey')
+# mo1 = heroRegex.search('Batman and Tina Fey.')
+# print(mo1.group())
+
+# mo2 = heroRegex.search('Tina Fey and Batman.')
+# print(mo2.group())
+
+# batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+# mo = batRegex.search('Batmobile lost a wheel')
+# print(mo.group())
+# print(mo.group(1))
+
+# optional matching with the question mark
+batRegex = re.compile(r'Bat(wo)?man')
+mo1 = batRegex.search('The Adventures of Batman')
 print(mo1.group())
 
-mo2 = heroRegex.search('Tina Fey and Batman.')
+mo2 = batRegex.search('The Adventures of Batwoman')
 print(mo2.group())
 
-batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
-mo = batRegex.search('Batmobile lost a wheel')
-print(mo.group())
-print(mo.group(1))
+phoneRegex = re.compile(r'(\d\d\d-)?\d\d\d-\d\d\d\d')
+mo1 = phoneRegex.search('My number is 415-555-4242')
+print(mo1.group())
+
+mo2 = phoneRegex.search('My number is 555-4242')
+print(mo2.group())
