36361a0cb4ac8fad183724a4400780b85a12ea92
Kunal Patel <kunalpatelx92@gmail.com>
2017-01-31 19:55:14 -0600

    Matching Everything with Dot-Star

--- Regular_Expressions/reModule.py
+++ Regular_Expressions/reModule.py
@@ -118,5 +118,19 @@ import re
 # print(wholeStringIsNum.search('12   34567890') == None)
 
 # the wildcard character
-atRegex = re.compile(r'.at')
-print(atRegex.findall('The cat in the hat sat on the flat mat.'))
+# atRegex = re.compile(r'.at')
+# print(atRegex.findall('The cat in the hat sat on the flat mat.'))
+
+# matching everything with Dot-Star
+nameRegex = re.compile(r'First Name: (.*) Last Name: (.*)')
+mo = nameRegex.search('First Name: Al Last Name: Sweigart')
+print(mo.group(1))
+print(mo.group(2))
+
+nongreedyRegex = re.compile(r'<.*?>')
+mo = nongreedyRegex.search('<To serve man> for dinner.>')
+print(mo.group())
+
+greedyRegex = re.compile(r'<.*>')
+mo = greedyRegex.search('<To serve man> for dinner.>')
+print(mo.group())
