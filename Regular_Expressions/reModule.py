0cdd682ec7a71c06673cc3e74868a47c13e99d6e
Kunal Patel <kunalpatelx92@gmail.com>
2017-01-31 19:15:25 -0600

    Greedy and Nongreedy Matching

--- Regular_Expressions/reModule.py
+++ Regular_Expressions/reModule.py
@@ -69,9 +69,18 @@ import re
 # print(mo3)
 
 # matching specific repetitions with curly brackets
-haRegex = re.compile(r'(Ha){3}')
-mo1 = haRegex.search('HaHaHa')
+# haRegex = re.compile(r'(Ha){3}')
+# mo1 = haRegex.search('HaHaHa')
+# print(mo1.group())
+
+# mo2 = haRegex.search('Ha')
+# print(mo2)
+
+# greedy and nongreedy matching
+greedyHaRegex = re.compile(r'(Ha){3,5}')
+mo1 = greedyHaRegex.search('HaHaHaHaHa')
 print(mo1.group())
 
-mo2 = haRegex.search('Ha')
-print(mo2)
+nongreedyHaRegex = re.compile(r'(Ha){3,5}?')
+mo2 = nongreedyHaRegex.search('HaHaHaHaHa')
+print(mo2.group())
