36c8b226217a21cdb78da17eb993d079f545ed73
Kunal Patel <kunalpatelx92@gmail.com>
2017-01-31 18:47:31 -0600

    Matching Multiple Groups with the Pipe

--- Regular_Expressions/reModule.py
+++ Regular_Expressions/reModule.py
@@ -6,14 +6,27 @@ import re
 # print('Phone number found: ' + mo.group())
 
 # grouping with parentheses
-phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
-mo = phoneNumRegex.search('My number is 415-555-4242.')
-print(mo.group(1))
-print(mo.group(2))
-print(mo.group(0))
-print(mo.group())
+# phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+# mo = phoneNumRegex.search('My number is 415-555-4242.')
+# print(mo.group(1))
+# print(mo.group(2))
+# print(mo.group(0))
+# print(mo.group())
 
-print(mo.groups())
-areaCode, mainNumber = mo.groups()
-print(areaCode)
-print(mainNumber)
+# print(mo.groups())
+# areaCode, mainNumber = mo.groups()
+# print(areaCode)
+# print(mainNumber)
+
+# matching multiple groups with the pipe
+heroRegex = re.compile(r'Batman|Tina Fey')
+mo1 = heroRegex.search('Batman and Tina Fey.')
+print(mo1.group())
+
+mo2 = heroRegex.search('Tina Fey and Batman.')
+print(mo2.group())
+
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+mo = batRegex.search('Batmobile lost a wheel')
+print(mo.group())
+print(mo.group(1))
