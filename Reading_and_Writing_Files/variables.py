c0a31b430f55873071e64fab30b2b8cb965cec5a
Kunal Patel <kunalpatelx92@gmail.com>
2017-02-27 17:37:21 -0600

    Saving Variables with the pprint.pformat() Function

--- Reading_and_Writing_Files/variables.py
+++ Reading_and_Writing_Files/variables.py
@@ -15,3 +15,18 @@ shelfFile =shelve.open('mydata')
 print(list(shelfFile.keys()))
 print(list(shelfFile.values()))
 shelfFile.close
+
+# Saving Variables with the pprint.pformat() Function
+import pprint
+
+dogs = [{"name": "Thor", "desc": "Golden Retriever"}, {"name": "Loki", "desc": "Husky"}]
+pprint.pformat(dogs)
+
+fileObj = open("myDogs.py", "w")
+fileObj.write('dogs = ' + pprint.pformat(dogs) + "\n")
+fileObj.close()
+
+import myDogs
+print(myDogs.dogs)
+print(myDogs.dogs[0])
+print(myDogs.dogs[0]["name"])
