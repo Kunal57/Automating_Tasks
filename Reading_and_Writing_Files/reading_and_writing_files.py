9d72c9759f84fdb7d67d42d6635f61064aeeaf08
Kunal Patel <kunalpatelx92@gmail.com>
2017-02-27 16:52:07 -0600

    Reading the Contents of Files

--- Reading_and_Writing_Files/reading_and_writing_files.py
+++ Reading_and_Writing_Files/reading_and_writing_files.py
@@ -1,3 +1,10 @@
 # Opening Files with the open() Function
 helloFile = open("hello.txt")
 print(helloFile)
+
+# Reading the Contents of Files
+helloContent = helloFile.read()
+print(helloContent)
+
+sonnetFile = open("sonnet29.txt")
+print(sonnetFile.readlines())
