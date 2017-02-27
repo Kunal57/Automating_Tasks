bcc154a3739210a21ccbe96523aa21123c85033e
Kunal Patel <kunalpatelx92@gmail.com>
2017-02-27 15:44:23 -0600

    The Current Working Directory

--- Reading_and_Writing_Files/file_paths.py
+++ Reading_and_Writing_Files/file_paths.py
@@ -1,6 +1,11 @@
-# Backslash on Windows and Forward Slash on OSX and Linux
 import os
 
-myFiles = ["accounts.txt", "details.csv", "invite.docx"]
-for filename in myFiles:
-  print(os.path.join("/Users/Kunal/", filename))
+# Backslash on Windows and Forward Slash on OSX and Linux
+# myFiles = ["accounts.txt", "details.csv", "invite.docx"]
+# for filename in myFiles:
+#   print(os.path.join("/Users/Kunal/", filename))
+
+# The Current Working Directory
+print(os.getcwd())
+os.chdir("/Users/Kunal/Development/Automating_Tasks")
+print(os.getcwd())
